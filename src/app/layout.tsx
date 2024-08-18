d6d79d846828cb1d3c20f239b008ee6a4586b616
BuildTools <unconfigured@null.spigotmc.org>
2024-08-19 01:23:42 +0700

    Fixed main app layout causing deployment errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,30 +2,24 @@ import type { Metadata } from "next";
 import { Comfortaa } from "next/font/google";
 
 import "./globals.css";
-export const comfortaa = Comfortaa({
+
+const comfortaa = Comfortaa({
     weight: "500",
     subsets: ["latin"],
 });
 
-interface Icon {
-  rel: string;
-  url: string;
-}
-
-interface MyMetadata extends Metadata {
-  icons: Icon[];
-}
-
 export const metadata: Metadata = {
     title: "scribe",
-    icons: [{ rel: 'icon', url: '/scribe.svg' }],
+    icons: {
+        icon: "/scribe.svg",
+    },
 };
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
     return (
         <html lang="en">
             <body className={comfortaa.className}>{children}</body>
